f7db75f72f3c65c35f31f33019cf8d240cec10da
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-25 11:27:54 -0300

    finalizado

---
+++ atividade 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace atividade_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[16];
+            Console.WriteLine("Digite 15 numeros");
+            
+
+            for (int i = 1; i < 16; i++){
+               Console.Write($"Numero {i}: ");
+               numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int a = 15; a > 0; a--){
+                Console.Write($"{numeros[a]}, ");
+            }
+        }
+    }
+}
